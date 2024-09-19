c19fd5433b11a1649281b1772922dcd38a4dcebf
Ivan Bazaga <ivan.cpweb@gmail.com>
2024-09-19 12:55:17 +0100

    .

---
+++ springBoot/urbanmanager/urbanmanager/src/test/java/com/urbanmanager/urbanmanager/UrbanmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.urbanmanager.urbanmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrbanmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
